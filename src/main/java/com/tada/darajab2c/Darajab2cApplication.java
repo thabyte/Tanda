85929e09569eaf35dd5033945410f96bdb71ac4c
Titus Muthomi <muthomititus+github@gmail.com>
2024-07-17 11:35:54 +0300

    Initial app set up

---
+++ src/main/java/com/tada/darajab2c/Darajab2cApplication.java
@@ -0,0 +1,13 @@
+package com.tada.darajab2c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Darajab2cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Darajab2cApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tada/darajab2c/Darajab2cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tada.darajab2c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Darajab2cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
